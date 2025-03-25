3442da07f5bd11425fad63b2f81cc9c17e996b3c
AleksandrSafarov <aleks.safarov2005@gmail.com>
2025-03-25 20:10:55 +0300

    .

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,87 +1,121 @@
-/**
- * Товар, приходящий от API (например, в ответ на GET /product/)
- */
-export interface IApiProduct {
-    productId: string;
-    productDescription: string;
-    productImage: string;
-    productTitle: string;
-    productCategory: string;
-    productPrice: number | null;
+// Типы для реализации базового класса Events
+export type EventName = string | RegExp;
+export type Subscriber = Function;
+export type EmitterEvent = {
+    eventName: string,
+    data: unknown
+};
+
+// Общие методы события
+export interface IEvents {
+  on<T extends object>(event: EventName, callback: (data: T) => void): void;
+  emit<T extends object>(event: string, data?: T): void;
+  trigger<T extends object>(event: string, context?: Partial<T>): (data: T) => void;
 }
 
-/**
- * Список товаров, возвращаемый с бэкенда
- */
-export interface IApiProductList {
-    totalCount: number;        // Общее количество товаров
-    productItems: IApiProduct[]; // Массив товаров
+// Данные ответа от сервера
+export type ApiListResponse<Type> = {
+  total: number,
+  items: Type[]
+};
+
+// Методы запросов к серверу
+export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
+
+// Данные обработчика валидности формы
+export interface IFormState {
+    valid: boolean;
+    errors: string[];
 }
 
-/**
- * Товар, используемый внутри приложения (UI).
- */
+// Данные контента для отрисовки внутри Модалки
+export interface IModalData {
+    content: HTMLElement;
+}
+
+// Описание методов для Api приложения
+export interface ILarekAPI {
+  getProductList: () => Promise<IProduct[]>;
+  getProductItem: (id: string) => Promise<IProduct>;
+  orderProducts: (order: IOrder) => Promise<IOrderResult>
+}
+
+// Данные товара
 export interface IProduct {
-    productId: string;
-    productDescription: string;
-    productImage: string;
-    productTitle: string;
-    productCategory: string;
-    productPrice: number | null;
+  id: string;
+  title: string;
+  price: number | null;
+  description: string;
+  category: string;
+  image: string;
 }
 
-/**
- * Данные покупателя, приходящие от API или отправляемые на бэкенд
- */
-export interface IApiCustomerData {
-    paymentMethod: string;
-    customerEmail: string;
-    customerPhone: string;
-    customerAddress: string;
+// Состояние приложения
+export interface IAppState {
+  catalog: IProduct[];
+  basket: IProduct[];
+  preview: string | null;
+  delivery: IDeliveryForm | null;
+  contact: IContactForm | null;
+  order: IOrder | null;
 }
 
-/**
- * Данные покупателя внутри приложения (UI).
- */
-export interface ICustomerData {
-    paymentMethod: string;
-    customerEmail: string;
-    customerPhone: string;
-    customerAddress: string;
+// Данные доставки
+export interface IDeliveryForm {
+  payment: string;
+  address: string;
 }
 
-/**
- * Тело запроса для создания заказа (POST /order)
- */
-export interface IApiOrderRequest {
-    paymentMethod: string;
-    customerEmail: string;
-    customerPhone: string;
-    customerAddress: string;
-    orderTotal: number;       // общая сумма заказа
-    productIds: string[];     // список ID товаров
+// Контактные данные
+export interface IContactForm {
+  email: string;
+  phone: string;
 }
 
-/**
- * Ответ от сервера при успешном создании заказа
- */
-export interface IApiOrderResponse {
-    orderId: string;    // идентификатор созданного заказа
-    confirmedTotal: number; // подтверждённая сумма заказа
+// Данные всего заказа
+export interface IOrder extends IDeliveryForm, IContactForm {
+  total: number;
+  items: string[];
 }
 
-/**
- * Структура ошибки при неудачном оформлении заказа (400 Bad Request)
- */
-export interface IApiOrderError {
-    errorMessage: string;
+// Данные ответа сервера на заказ
+export interface IOrderResult {
+  id: string;
+  total: number;
 }
 
-/**
- * Заказ, используемый внутри приложения (UI).
- */
-export interface IOrder {
-    orderedProducts: IProduct[];     // товары, добавленные в корзину
-    customerInfo: ICustomerData; // данные покупателя
-    orderTotal: number;            // итоговая сумма заказа
+// Ошибки Форм
+export type FormErrors = Partial<Record<keyof IOrder, string>>;
+
+// Данные для отображения карточки
+export interface ICard extends IProduct{
+  index?: string;
+  buttonTitle? : string;
+}
+
+// Данные для отображения корзиный
+export interface IBasketView {
+  items: HTMLElement[];
+  total: number;
+}
+
+// Данный для отображения главной страницы
+export interface IPage{
+  counter: number;
+  catalog: HTMLElement[];
+}
+
+//Данные для отображения успешного заказа
+export interface ISuccess {
+  total: number;
+}
+
+// Действия передаваемые в конструктор
+export interface IActions {
+  onClick: (event: MouseEvent) => void;
+}
+
+// Действия передаваемые в конструктор успешного заказа
+export interface ISuccessActions {
+  onClick: () => void;
 }
